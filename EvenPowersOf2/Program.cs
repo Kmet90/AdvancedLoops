ceea3f70b26119a69104749ea708da4af8df5ea5
Kmet90 <kmet90@gmail.com>
2017-11-04 09:27:15 +0000

    Advanced loops

---
+++ EvenPowersOf2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenPowersOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            if (n == 1) Console.WriteLine(n);
+            else
+            {
+                Console.WriteLine(num);
+                for (int i = 0; i < n; i += 2)
+                {
+                    num = num * 4;
+                    Console.WriteLine(num);
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ NumberInRange1to100/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberInRange1to100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number in range [1...100]:");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n < 1 || n > 100)
+            {
+                Console.WriteLine("Invalid nimber!");
+                Console.Write("Enter number in range [1...100]:");
+                n = int.Parse(Console.ReadLine());
+            }
+                Console.WriteLine("Nimber is :{0}",n);
+        }
+    }
+}
